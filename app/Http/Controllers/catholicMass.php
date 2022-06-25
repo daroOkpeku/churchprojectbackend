4f9ce804267b97bd93a4f853ad10203ee0fea231
daroOkpeku <stephenjason41@gmail.com>
2022-06-25 11:10:08 +0100

    church

--- app/Http/Controllers/catholicMass.php
+++ app/Http/Controllers/catholicMass.php
@@ -59,7 +59,7 @@ class catholicMass extends Controller
     public function read($read){
     // 2022-07-02
        $test_arr  = explode('-', $read);
-       if(checkdate($test_arr[2], $test_arr[1],  $test_arr[0])){
+       if(ctype_digit(implode('',$test_arr))){
         return true;
        }else{
         return false;
